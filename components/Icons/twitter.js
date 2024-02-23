4b44846b0f1a1e0393e044c498ecacea5ec3cc8f
yared abebe leake <yaredaleake@gmail.com>
2024-02-22 22:20:35 -0700

    Update twitter.js

--- components/Icons/twitter.js
+++ components/Icons/twitter.js
@@ -3,15 +3,15 @@ const IconTwitter = () => (
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
+    viewBox="0 0 512 512"
+    fill="#fff"
+    stroke="#fff"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="feather feather-twitter"
+    className="fa-brands fa-x-twitter"
   >
-    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
   </svg>
 );
 
